9750d73a662cd74927b85f64d50ae461f842b50e
Christopher Carr <ccarr@itcollege.ee>
2016-09-29 13:34:01 +0000

    Changed code for correct switching

--- Andris.py
+++ Andris.py
@@ -15,17 +15,17 @@ GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 
-# Blink some leds
-# while True:
-#     GPIO.output(red, False)
-#     sleep(1)  # Sleep for 1 second
-#     GPIO.output(red, False)
-#     GPIO.output(blue, True)
-#     sleep(1)
-#     GPIO.output(blue, False)
-#     GPIO.output(green, True)
-#     sleep(1)
-#     GPIO.output(green, False)
+ Blink some leds
+while True:
+     GPIO.output(red, False)
+     sleep(1)  # Sleep for 1 second
+     GPIO.output(red, True)
+     GPIO.output(blue, False)
+     sleep(1)
+     GPIO.output(blue, True)
+     GPIO.output(green, False)
+     sleep(1)
+     GPIO.output(green, True)
 def cleanLed():
   GPIO.output(red, True)
   GPIO.output(blue, True)
